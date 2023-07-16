d0b65a274650db3a552c73bfe3bb8985dfa798eb
Robin Fauchery <robin.fauchery.rf@gmail.com>
2023-07-16 17:47:01 +0200

    Commit changes

--- theme-ESGI/search.php
+++ theme-ESGI/search.php
@@ -2,18 +2,20 @@
 
     <main class="container-search">
         <div>
-        <h1><?= get_theme_mod('resultat_recherche', "Search results for: ") . get_search_query() ?></h1>
-        <?php if (have_posts()) : ?>
+            <h1><?= get_theme_mod('resultat_recherche', "Search results for: ") . get_search_query() ?></h1>
+            <div class="article-list">
+            <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
-                <article>
+                <article class="article-card">
                     <h4><a href="<?= the_permalink() ?>"><?= the_title() ?></a></h4>    
-                    <p><?= get_the_category_list(', ') . ', ' . get_the_date() ?></p>
-                    <p><?= the_excerpt() ?></p>
+                    <p class="info-post"><?= get_the_category_list(', ') . ', ' . get_the_date() ?></p>
+                    <?= the_excerpt() ?>
                 </article>
             <?php endwhile; ?>
         <?php else : ?>
             <p class="p26"><?= get_theme_mod('text_case_no_result', "No result.")?></p>
         <?php endif; ?> 
+            </div>
         </div>
     </main>
 <?php get_footer(); ?>
